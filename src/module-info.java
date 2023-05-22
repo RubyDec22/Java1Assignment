fa34fbb7bdb22ca49ed337dee583312ccad15670
Ruby K <rubykdec22@gmail.com>
2023-05-22 17:53:07 -0500

    Commited

---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author hanis
+ *
+ */
+module Assigment1 {
+}
---
+++ src/session1/Helloruby.java
@@ -0,0 +1,10 @@
+package session1;
+
+public class Helloruby {
+
+	public static void main(String[] args) {
+		System.out.println("Hello");
+		System.out.println("my name is Ruby");
+	}
+
+}
